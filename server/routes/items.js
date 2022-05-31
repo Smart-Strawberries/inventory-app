68f6e0360b8cb973899ee9f03ab9d1f7151a0693
Jen Perez <93927905+jenperez613@users.noreply.github.com>
2022-05-31 17:42:29 -0400

    Postman working for all items route + added NS's seedData

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { Sauce } = require("../models");
+const { Item } = require("../models");
 
-// GET /sauce
+
 router.get("/", async (req, res, next) => {
   try {
-    const sauces = await Sauce.findAll();
-    res.send(sauces);
+    const items = await Item.findAll();
+    res.send(items);
   } catch (error) {
     next(error);
   }
 });
+
+module.exports = router;
 
 module.exports = router;
